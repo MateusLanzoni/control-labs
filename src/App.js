a53a426b5061f27bc0973917e7e4d8bdb1a51eba
Fermc1882 <fernandomesino91@gmail.com>
2024-08-21 21:29:22 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login/login";
 import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <Footer/>
+
     </div>
   );
 }
