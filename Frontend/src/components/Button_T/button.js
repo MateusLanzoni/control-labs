5ce59533c4c6bf2f5e4eea91457453576d13b845
OKJuanes <juan.castano68@eia.edu.co>
2024-09-08 18:13:12 -0500

    imagenes

--- Frontend/src/components/Button_T/button.js
+++ Frontend/src/components/Button_T/button.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Square_0 from '../Square_0/Square_0';
 import Square_3 from '../Square_3/Square_3';
+import BImg0 from '../../assets/piso0.png'; // Imagen del botón inicial
+import BImg3 from '../../assets/piso3.png'; // Imagen del botón cuando está toggled
+
 import './button.css';
 
 const Button_T = () => {
@@ -13,12 +16,16 @@ const Button_T = () => {
   return (
     <div>
       {isToggled ? (
-        <Square_3 /> // Renderiza el primer componente
+        <Square_3 /> // Renderiza el componente Square_3
       ) : (
-        <Square_0 /> // Renderiza el segundo componente
+        <Square_0 /> // Renderiza el componente Square_0
       )}
-      <button onClick={toggleState}>
-        {isToggled ? 'Show First Component' : 'Show Second Component'}
+      <button onClick={toggleState} className="toggle-button">
+        {/* Cambia la imagen del botón según el estado */}
+        <img
+          src={isToggled ? BImg3 : BImg0} // Cambia la imagen según el estado
+          alt="Toggle Button"
+        />
       </button>
     </div>
   );
