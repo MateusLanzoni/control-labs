0e3979d1bf612a0cdc864c6925b38129cce90898
Mateus Lanzoni Trejos <mateus.lanzoni@eia.edu.co>
2024-10-16 19:36:45 -0500

    add UserInfoPage component with Navbar and Footer

---
+++ Frontend/src/pages/UserInfoPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/NavBar/navbar';
+import Footer from '../components/Footer/footer';
+
+function UserInfoPage() {
+  return (
+    <div className="main-container">
+      <Navbar />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default UserInfoPage;
